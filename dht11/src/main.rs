2cce090987c70a8bf762adefb81e93fc50846e99
Mll <mllcms@163.com>
2024-05-02 19:49:59 +0800

    fix: 使用本地库

--- dht11/src/main.rs
+++ dht11/src/main.rs
@@ -1,7 +1,7 @@
 #![no_std]
 #![no_main]
 
-use dht_embedded::{DhtSensor, NoopInterruptControl};
+use driver::dht11::Dht11;
 use esp_backtrace as _;
 use esp_hal::{clock::ClockControl, delay::Delay, gpio::IO, peripherals::Peripherals, prelude::*};
 
@@ -17,10 +17,10 @@ fn main() -> ! {
 
     let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
     let dht11 = io.pins.gpio15.into_open_drain_output();
-    let mut dht11 = dht_embedded::Dht11::new(NoopInterruptControl, delay.clone(), dht11);
+    let mut dht11 = Dht11::new(dht11, delay);
     loop {
         match dht11.read() {
-            Ok(reading) => log::info!("{}°C, {}% RH", reading.temperature(), reading.humidity()),
+            Ok(reading) => log::info!("{reading}"),
             Err(e) => log::error!("Error: {}", e),
         }
         delay.delay(500.millis());
