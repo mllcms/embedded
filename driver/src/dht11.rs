311ede7b693a6a6565ff2e53ee0972b407e6ed65
Mll <mllcms@163.com>
2024-05-02 14:38:12 +0800

    feat: 新增 dht11 驱动

---
+++ driver/src/dht11.rs
@@ -0,0 +1,137 @@
+use core::{
+    fmt,
+    fmt::{Debug, Display},
+};
+
+use embedded_hal::{
+    delay::DelayNs,
+    digital::{InputPin, OutputPin},
+};
+
+
+pub struct Dht11<HE, P, D>
+where
+    P: InputPin<Error = HE> + OutputPin<Error = HE>,
+    D: DelayNs,
+{
+    pin:   P,
+    delay: D,
+}
+
+impl<HE, P, D> Dht11<HE, P, D>
+where
+    P: InputPin<Error = HE> + OutputPin<Error = HE>,
+    D: DelayNs,
+{
+    pub fn new(pin: P, delay: D) -> Self {
+        Self { pin, delay }
+    }
+
+    pub fn read(&mut self) -> Result<Reading, DhtError<HE>> {
+        let mut buf = [0_u8; 5];
+        // 重启 dht11
+        self.pin.set_low()?;
+        self.delay.delay_us(3000);
+        self.pin.set_high()?;
+        self.delay.delay_us(25);
+
+        // 等 dht11 信号
+        self.wait_high(85)?;
+        self.wait_low(85)?;
+
+        // 开始接收数据
+        for bit in 0..40 {
+            self.wait_high(55)?;
+
+            let elapsed = self.wait_low(70)?;
+            if elapsed > 30 {
+                let byte = bit / 8;
+                let shift = 7 - bit % 8;
+                buf[byte] |= 1 << shift;
+            }
+        }
+        // 校验数据
+        let checksum = (buf[0..4].iter().fold(0u16, |acc, next| acc + *next as u16) & 0xff) as u8;
+        if buf[4] != checksum {
+            return Err(DhtError::ChecksumMismatch(buf[4], checksum));
+        }
+
+        // 湿度范围在 0%-100%
+        let humidity = buf[0] as f32 + buf[1] as f32 / 10.0;
+        if !(0.0..100.0).contains(&humidity) {
+            return Err(DhtError::InvalidData);
+        }
+
+        // 符号位有1为零下温度
+        let mut temperature = buf[2] as f32 + buf[3] as f32 / 10.0;
+        if buf[2] & 0x80 != 0 {
+            temperature = -temperature;
+        }
+
+        Ok(Reading { humidity, temperature })
+    }
+
+    pub fn wait_low(&mut self, timeout_us: u32) -> Result<u32, DhtError<HE>> {
+        for i in 0..timeout_us {
+            if self.pin.is_low()? {
+                return Ok(i);
+            }
+            self.delay.delay_us(1);
+        }
+        Err(DhtError::Timeout)
+    }
+
+    pub fn wait_high(&mut self, timeout_us: u32) -> Result<u32, DhtError<HE>> {
+        for i in 0..timeout_us {
+            if self.pin.is_high()? {
+                return Ok(i);
+            }
+            self.delay.delay_us(1);
+        }
+        Err(DhtError::Timeout)
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Reading {
+    pub humidity:    f32,
+    pub temperature: f32,
+}
+
+impl Display for Reading {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:.1}°C  {:.1}%RH", self.temperature, self.humidity)
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum DhtError<HE> {
+    ChecksumMismatch(u8, u8),
+    PinError(HE),
+    InvalidData,
+    NotPresent,
+    Timeout,
+}
+
+impl<HE> From<HE> for DhtError<HE> {
+    fn from(error: HE) -> Self {
+        DhtError::PinError(error)
+    }
+}
+
+impl<HE: Debug> Display for DhtError<HE> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use DhtError::*;
+        match self {
+            ChecksumMismatch(expected, calculated) => write!(
+                f,
+                "Data read was corrupt (expected checksum {:x}, calculated {:x})",
+                expected, calculated
+            ),
+            NotPresent => write!(f, "DHT device not found"),
+            InvalidData => f.write_str("Received data is out of range"),
+            Timeout => f.write_str("Timed out waiting for a read"),
+            PinError(err) => write!(f, "HAL pin error: {:?}", err),
+        }
+    }
+}
